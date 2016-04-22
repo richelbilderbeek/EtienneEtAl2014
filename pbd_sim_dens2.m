789b9fc8f8215db68c0a9307ec123a6aad859e7b
richelbilderbeek <richel@richelbilderbeek.nl>
2016-04-22 09:20:23 +0200

    Added simulated data, indented some code

--- pbd_sim_dens2.m
+++ pbd_sim_dens2.m
@@ -1,49 +1,49 @@
 function [results] = pbd_sim_dens2(runoncluster,mui,la2i,agei,seed,startmc,endmc)
-% Simulating number good and incipient species
-if ischar(runoncluster) runoncluster = str2num(runoncluster); end;
-if ischar(mui) mui = str2num(mui); end;
-if ischar(la2i) la2i = str2num(la2i); end;
-if ischar(agei) agei = str2num(agei); end;
-if ischar(startmc) startmc = str2num(startmc); end;
-if ischar(endmc) endmc = str2num(endmc); end;
-if ischar(seed) seed = str2num(seed); end;
-if runoncluster == 1
-    outputdir = '';
-else
-    outputdir = 'd:\data\ms\posterior\';
-end;
-rand('twister',seed);
-results = zeros(endmc,5);
-la1 = 0.5;
-muvec = [0,0.1,0.2];
-la2vec = [0.1,0.3,1,0.03];
-agevec = [5,10,15];
-stem_or_crown = 1;
-eps = 1E-6;
-outfilename2 = sprintf('%spbd_sim_dens2_%d-%d-%d.out',outputdir,mui,la2i,agei);
-for mc = 1:endmc  
-    disp(mc);
-    la2 = la2vec(la2i);
-    mu = muvec(mui);
-    age = agevec(agei);
-    pars = [la1,mu,la2,age];
-    tssim = durspec(log(pars));
-    times = [];
-    for ii = 0:stem_or_crown
-        valid = 0;
-        while not(valid)
-           [ti,tc,ig,pa,valid] = pbd_sim_recstr_full(pars);
-        end;        
-        te = zeros(1,length(ti));
-        te(1:sum(ig~=0)) = -1;
-        [times1,~] = pbd_sim_step2a(ti,tc,te,pa);        
-        times = [times,age-times1(times1>0)];        
-    end;
-    times = [age,times];
-    times = sort(times(times > 0),'descend');
-    outfile2 = fopen(outfilename2,'at');
-    fprintf(outfile2,'%0.6f ',times);
-    fprintf(outfile2,'\n');
-    fclose(outfile2);
-end;
+  % Simulating number good and incipient species
+  if ischar(runoncluster) runoncluster = str2num(runoncluster); end;
+  if ischar(mui) mui = str2num(mui); end;
+  if ischar(la2i) la2i = str2num(la2i); end;
+  if ischar(agei) agei = str2num(agei); end;
+  if ischar(startmc) startmc = str2num(startmc); end;
+  if ischar(endmc) endmc = str2num(endmc); end;
+  if ischar(seed) seed = str2num(seed); end;
+  if runoncluster == 1
+      outputdir = '';
+  else
+      outputdir = 'd:\data\ms\posterior\';
+  end;
+  rand('twister',seed);
+  results = zeros(endmc,5);
+  la1 = 0.5;
+  muvec = [0,0.1,0.2];
+  la2vec = [0.1,0.3,1,0.03];
+  agevec = [5,10,15];
+  stem_or_crown = 1;
+  eps = 1E-6;
+  outfilename2 = sprintf('%spbd_sim_dens2_%d-%d-%d.out',outputdir,mui,la2i,agei);
+  for mc = 1:endmc  
+      disp(mc);
+      la2 = la2vec(la2i);
+      mu = muvec(mui);
+      age = agevec(agei);
+      pars = [la1,mu,la2,age];
+      tssim = durspec(log(pars));
+      times = [];
+      for ii = 0:stem_or_crown
+          valid = 0;
+          while not(valid)
+             [ti,tc,ig,pa,valid] = pbd_sim_recstr_full(pars);
+          end;        
+          te = zeros(1,length(ti));
+          te(1:sum(ig~=0)) = -1;
+          [times1,~] = pbd_sim_step2a(ti,tc,te,pa);        
+          times = [times,age-times1(times1>0)];        
+      end;
+      times = [age,times];
+      times = sort(times(times > 0),'descend');
+      outfile2 = fopen(outfilename2,'at');
+      fprintf(outfile2,'%0.6f ',times);
+      fprintf(outfile2,'\n');
+      fclose(outfile2);
+  end;
 end
--- pbdbd.R
+++ pbdbd.R
@@ -1,3 +1,7 @@
+# Do maximum likelihood parameter estimation on 'pbd_sim_dens2_' files,
+# which contains the branching times of simulated PBD phylogenies.
+# Write the parameter estimates to 'pbdbd.Rout'
+
 library(laser)
 library(TESS)
 library(DDD)
@@ -5,11 +9,15 @@ library(PBD)
 library(subplex)
 muvec = c(0,0.1,0.2)
 la2vec = c(0.1,0.3,1)
-for(i in 1:3) { for(j in 1:3) { for(k in 1:1000) {
-   datafile = paste('pbd_sim_dens2_',i,'-',j,'-3.out',sep = '')
-   data = scan(file = datafile,skip = k - 1, nlines = 1)
-   flush.console()
-   pars1 = c(0.5,muvec[i],la2vec[j],muvec[i])
-   out1 = bd_ML(brts = data,initparsopt = c(pars1[1]*pars1[3]/(pars1[3]+pars1[4]),pars1[2] + 0.01),cond = 1,soc = 2,btorph = 0)
-   write(c(pars1,k,length(data)+1,as.numeric(unlist(out1))),file = 'pbdbd.Rout',ncolumns = 13, append = T, sep = ',')
-}}}
+for(i in 1:3) { 
+  for(j in 1:3) { 
+    for(k in 1:1000) {
+      datafile = paste('pbd_sim_dens2_',i,'-',j,'-3.out',sep = '')
+      data = scan(file = datafile,skip = k - 1, nlines = 1)
+      flush.console()
+      pars1 = c(0.5,muvec[i],la2vec[j],muvec[i])
+      out1 = bd_ML(brts = data,initparsopt = c(pars1[1]*pars1[3]/(pars1[3]+pars1[4]),pars1[2] + 0.01),cond = 1,soc = 2,btorph = 0)
+      write(c(pars1,k,length(data)+1,as.numeric(unlist(out1))),file = 'pbdbd.Rout',ncolumns = 13, append = T, sep = ',')
+    }
+  }
+}
